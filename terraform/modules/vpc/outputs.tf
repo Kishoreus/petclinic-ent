16d68cdc06349e4e6d28242f1ba1857d6ecb3c66
Kishoreus <137713866+Kishoreus@users.noreply.github.com>
2026-08-30 23:35:03 +0530

    Add outputs for eice security group and EC2 instance

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -13,3 +13,11 @@ output "private_subnet_ids" {
 output "all_subnet_ids" {
   value = concat(aws_subnet.public[*].id, aws_subnet.private[*].id)
 }
+
+output "eice_security_group_id" {
+  value = aws_security_group.eice.id
+}
+
+output "eice_id" {
+  value = aws_ec2_instance_connect_endpoint.eks.id
+}
